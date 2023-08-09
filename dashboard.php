2ba785c05f9e4ad7c9b8dd3cb990004e66382502
Pasindu Rangana <115542129+PasinduRR@users.noreply.github.com>
2023-08-10 00:30:03 +0530

    new

--- dashboard.php
+++ dashboard.php
@@ -10,9 +10,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="assests/style/dashboard.css">
+    <link rel="stylesheet" href="airTicket.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-<div class="profile-card-container">
+    <div class="profile-card-container">
         <?php
             $email = $_SESSION['username'];
             $sql = "SELECT * FROM users WHERE email = '$email'";
@@ -64,7 +68,180 @@
 
                     EOL;
             }
+        ?>
+    </div>
+
+    <div class="flight-cart-container">
+        <?php
+            $email = $_SESSION['username'];
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_assoc($result);
+            $fname = strtoupper($row['first_name']);
+            $lname = strtoupper($row['last_name']);
+            $uid = $row['user_id'];
+
+            $sql = "SELECT * FROM ticket WHERE uid = '{$uid}'";
+            $result = mysqli_query($conn, $sql);
+
+            $fid_array = array();
+
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)){
+                    $fid = $row['fid'];
+                    array_push($fid_array, $fid);
+                }
+            }
+
+            $length = count($fid_array);
+            for ($i = 0; $i < $length; $i++) {
+                $fid = $fid_array[$i];
+
+                $sql = "SELECT * FROM flight WHERE flight_id = '{$fid}'";
+                $result = mysqli_query($conn, $sql);
+                
+                $row = mysqli_fetch_assoc($result);
+
+                        $id = $row['flight_id'];
+                        $departure = $row['departure_time'];
+                        $arrival = $row['arrival_time'];
+                        $from = strtoupper($row['depature']);
+                        $to = strtoupper($row['destination']);
+                        $duration = $row['duration'];
+                        $hours = intdiv($duration, 60);
+                        $minutes = $duration % 60;
+                        $price = $row['price'];
+                        $boarding = explode(" ", $departure);
+                        $date = $boarding[0];
+                        $boarding_time = $boarding[1];
+                        $departure_time = explode(" ", $arrival)[1];
+
+                        echo <<<EOL
+
+
+                        <div class="flight-ticket-card">
+                            <div class="flight-ticket-header">
+                                <div class="air-ticket-label">
+                                    AIR TICKET
+                                </div>
+                                <div class="boarding-pass-label">
+                                    BOARDING PASS
+                                </div>
+                            </div>
+                            <div class="flight-ticket-information-container">
+
+                                <div class="information-upper">
+                                    <div class="name-container">
+                                        <div class="passenger-name-label">
+                                            NAME OF PASSANGER
+                                        </div>
+                                        <div class="passenger-name">
+                                            {$fname} {$lname}
+                                        </div>
+                                    </div>
+                                    <div class="date-container">
+                                        <div class="date-label">
+                                            DATE
+                                        </div>
+                                        <div class="date">
+                                            {$date}
+                                        </div>
+                                    </div>
+                                    <div class="boarding-time-container">
+                                        <div class="boarding-time-label">
+                                            BOARDING TIME
+                                        </div>
+                                        <div class="boarding-time">
+                                            {$departure_time}
+                                        </div>
+                                    </div>
+                                    <div class="departure-time-container">
+                                        <div class="departure-time-label">
+                                            DEPARTURE TIME
+                                        </div>
+                                        <div class="passenger-name">
+                                            {$boarding_time}
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="information-middle">
+                                    <div class="from-to-container">
+                                        <div class="from-container">
+                                            <div class="from-label">
+                                                FROM
+                                            </div>
+                                            <div class="from">
+                                                {$from}
+                                            </div>
+                                        </div>
+                                        <div class="to-container">
+                                            <div class="to-label">
+                                                TO
+                                            </div>
+                                            <div class="to">
+                                                {$to}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="empty-container">
+
+                                    </div>
+                                    <div class="duration-container">
+                                        <div class="duration-label">
+                                            DURATION
+                                        </div>
+                                        <div class="duration">
+                                            {$hours} HOURS {$minutes} MINUTES
+                                        </div>
+                                    </div>
+                                    <div class="qr-code-container">
+                                        <img class="qr-code" id="resultImage" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=bahukapn" alt="Result Image">
+                                    </div>
+                                </div>
+
+                                <div class="information-bottom">
+                                    <div class="flight-id-container">
+                                        <div class="flight-id-label">
+                                            FLIGHT ID :
+                                        </div>
+                                        <div class="flight-id">
+                                            F{$fid}
+                                        </div>
+                                    </div>
+                                    <div class="ticket-id-container">
+                                        <div class="flight-id-label">
+                                            TICKET ID :
+                                        </div>
+                                        <div class="ticket-id">
+                                            TXX
+                                        </div>
+                                    </div>
+                                    <div class="gate-close-notification">
+                                        GATE CLOSED 30 MINUTES BEFORE DEPARTURE
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flight-ticket-footer">
+                                <div class="footer-color-ribben"></div>
+                                <div class="company-name-container">
+                                    <div class="company-name">
+                                        FLIGHT NAV PRO
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        
+
+                        EOL;
+
+            } 
+
+            
             ?>
+    </div>
 </body>
 </html>
 
--- ticketBuy.php
+++ ticketBuy.php
@@ -15,15 +15,38 @@
 <div class="form-container">
     <form class="search-form" action="ticketBuy.php" method="get">
         <div class="inputBoxContainer">
-            <input class="inputBox" placeholder="Depature" name="depature" id="depature" type="text">
+            <input id="departure" class="inputBox" placeholder="Depature" name="depature" id="depature" type="text">
         </div>
         <div class="inputBoxContainer">
-            <input class="inputBox" placeholder="Destination" name="destination" id="destination" type="text">
+            <input id="destination" class="inputBox" placeholder="Destination" name="destination" id="destination" type="text">
         </div>
         <input class="btnSearch" type="submit" name="search" value="Search">
     </form>
 </div>
 
+<div class="overlay" id="overlay"></div>
+
+<div class="popup-container" id="popup">
+    <div class="image-container">
+        <img class="tick-img" src="assests/images/tick.png">
+    </div>
+    <div class="popup-header">Thank You!</div>
+    <div class="popup-message">Your details has been successfully submitted.</div>
+    <div class="button-container">
+        <button class="close-button" type="button" onclick="closePopup()">Close</button>
+    </div>
+</div>
+
+<script>
+    let popup = document.getElementById('popup');
+    let overlay = document.getElementById('overlay');
+
+    function closePopup(){
+        popup.style.visibility = 'hidden';
+        overlay.style.visibility = 'hidden';
+    }
+
+</script>
 
 <?php
     $result_count = 0;
@@ -32,12 +55,199 @@
         $departure = filter_input(INPUT_GET, "depature", FILTER_SANITIZE_SPECIAL_CHARS);
         $destination = filter_input(INPUT_GET, "destination", FILTER_SANITIZE_SPECIAL_CHARS);
 
-        if(empty($departure)){
-            echo '<script>alert("enter a departure");</script>';
+        if(empty($departure) && empty($destination)){
+            echo <<<EOL
+                    <script>
+                        var inputElement = document.getElementById("departure");
+                        inputElement.placeholder = "Enter a departure";
+                        inputElement.classList.add("empty-warning");
+
+                        var inputElement = document.getElementById("destination");
+                        inputElement.placeholder = "Enter a destination";
+                        inputElement.classList.add("empty-warning");
+                    </script>;
+                    EOL;
         }
-        elseif(empty($destination)){
-            echo '<script>alert("enter a destination");</script>';
+        elseif(!empty($departure) && empty($destination)){
+            $sql = "SELECT * FROM flight WHERE depature = '$departure'";
+
+            try{
+                $result = mysqli_query($conn, $sql);
+
+                echo '<div class="flight-cart-container">';
+                $result_count = mysqli_num_rows($result);
+
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+    
+                        $id = $row['flight_id'];
+                        $departure = $row['departure_time'];
+                        $arrival = $row['arrival_time'];
+                        $from = $row['depature'];
+                        $to = $row['destination'];
+                        $duration = $row['duration'];
+                        $hours = intdiv($duration, 60);
+                        $minutes = $duration % 60;
+                        $price = $row['price'];
+    
+                        echo <<<EOL
+    
+                            <div class="flight-card">
+                                <div class="flight-information-container">
+
+                                    <div class="container">
+                                        <div class="label">Flight ID</div>
+                                        <div class="id">: {$id}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Arrival Time</div>
+                                        <div class="arrival-time">: {$arrival}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">departure Time</div>
+                                        <div class="departure-time">: {$departure}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">From</div>
+                                        <div class="from-time">: {$from}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">To</div>
+                                        <div class="to">: {$to}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Duration</div>
+                                        <div class="duration">: {$hours} Hours {$minutes} Minutes</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Price</div>
+                                        <div class="price">: {$price}.00 LKR</div>
+                                    </div>
+
+                                </div>
+
+                            EOL;
+
+                        echo <<<EOL
+                            
+                            <div class="buy-button-container">
+                                <form action="ticketBuy.php" method="post">
+                                    <input type="hidden" value="{$id}" name="flight-id">
+                                    <input type="submit" class="buy-now-button" name="submit" value="BOOK NOW">
+                                </form>
+                            </div>
+
+                        </div>
+    
+                        EOL;
+
+                        
+    
+                    }
+
+                }
+                else{
+                    echo '<script>alert("No flight available!");</script>';
+                }
+
+                echo "</div>";
+            }
+            catch(mysqli_sql_exception $e){
+                $error =  $e->getMessage();
+                echo '<script>alert("' . $error . '");</script>';
+            }
         }
+
+        elseif(empty($departure) && !empty($destination)){
+            $sql = "SELECT * FROM flight WHERE destination = '$destination'";
+
+            try{
+                $result = mysqli_query($conn, $sql);
+
+                echo '<div class="flight-cart-container">';
+                $result_count = mysqli_num_rows($result);
+
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+    
+                        $id = $row['flight_id'];
+                        $departure = $row['departure_time'];
+                        $arrival = $row['arrival_time'];
+                        $from = $row['depature'];
+                        $to = $row['destination'];
+                        $duration = $row['duration'];
+                        $hours = intdiv($duration, 60);
+                        $minutes = $duration % 60;
+                        $price = $row['price'];
+    
+                        echo <<<EOL
+    
+                            <div class="flight-card">
+                                <div class="flight-information-container">
+
+                                    <div class="container">
+                                        <div class="label">Flight ID</div>
+                                        <div class="id">: {$id}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Arrival Time</div>
+                                        <div class="arrival-time">: {$arrival}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">departure Time</div>
+                                        <div class="departure-time">: {$departure}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">From</div>
+                                        <div class="from-time">: {$from}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">To</div>
+                                        <div class="to">: {$to}</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Duration</div>
+                                        <div class="duration">: {$hours} Hours {$minutes} Minutes</div>
+                                    </div>
+                                    <div class="container">
+                                        <div class="label">Price</div>
+                                        <div class="price">: {$price}.00 LKR</div>
+                                    </div>
+
+                                </div>
+
+                            EOL;
+
+                        echo <<<EOL
+                            
+                            <div class="buy-button-container">
+                                <form action="ticketBuy.php" method="post">
+                                    <input type="hidden" value="{$id}" name="flight-id">
+                                    <input type="submit" class="buy-now-button" name="submit" value="BOOK NOW">
+                                </form>
+                            </div>
+
+                        </div>
+    
+                        EOL;
+
+                        
+    
+                    }
+
+                }
+                else{
+                    echo '<script>alert("No flight available!");</script>';
+                }
+
+                echo "</div>";
+            }
+            catch(mysqli_sql_exception $e){
+                $error =  $e->getMessage();
+                echo '<script>alert("' . $error . '");</script>';
+            }
+        }
+
         else{
             $sql = "SELECT * FROM flight WHERE depature = '$departure' AND destination = '$destination'";
 
@@ -236,7 +446,11 @@
             $sql = "INSERT INTO ticket (uid,fid) VALUES ($uid,$fid);";
             mysqli_query($conn, $sql);
 
-            echo '<script>alert("Congratulation!");</script>';
+            echo <<<EOT
+                <script>document.getElementById('popup').style.visibility='visible';
+                document.getElementById('overlay').style.visibility='visible';
+                </script>
+                EOT;
 
         }
         catch(mysqli_sql_exception $e){
@@ -249,11 +463,14 @@
     
 ?>
 
+
 </body>
-</html>
 
 <script>
+
     var element = document.getElementById("ticket-button");
     element.style.fontWeight = "bold";
+
 </script>
 
+</html>
