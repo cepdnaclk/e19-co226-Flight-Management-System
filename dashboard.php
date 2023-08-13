d2be02013f41b1395a39ff88866bc5c5021e1155
Akash Withanaarachchi <e19453@eng.pdn.ac.lk>
2023-08-13 20:55:06 +0530

    ticket buy page overlay not displaying properly bug fixed

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="assests/style/dashboard.css">
-    <link rel="stylesheet" href="airTicket.css">
+    <link rel="stylesheet" href="assests/style/airTicket.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
